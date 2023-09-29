49726d4d266d4926d5a93e81cd9a2a37bbd76417
SakshiDubile <121311677+SakshiDubile@users.noreply.github.com>
2023-09-29 09:55:31 +0530

    Add files via upload

---
+++ day-11-p2/main.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row,col,n;
+    cout<<"Enter the number of rows you want:";
+    cin>>n;
+    for(row=1;row<=n;row++){
+        for(col=row;col<n;col++){
+            cout<<" ";
+        }
+        for(col=1;col<=row;col++){
+            cout<<col;
+        }
+        for(col=row-1;col>=1;col--){
+            cout<<col;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
