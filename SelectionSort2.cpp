158bfd0284a1a077f06743f443a1a75f4b7fd4ef
SakshiDubile <121311677+SakshiDubile@users.noreply.github.com>
2023-10-16 00:02:43 +0530

    Add files via upload

---
+++ SelectionSort2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int arr[100],n,i,j,index;
+	cout<<"Enter the size of the array:";
+	cin>>n;
+	cout<<"Enter the elements:"<<endl;
+	for(i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(j=0;j<n;j++){
+		cout<<arr[j]<<"\t";
+	}
+	for(i=n-1;i>=0;i--){
+		index=i;
+		for(j=i-1;j>=0;j--){
+			if(arr[j]>arr[index]){
+				index=j;
+			}
+			
+		}
+		swap(arr[i],arr[index]);
+	}
+	cout<<endl;
+	cout<<"Sorted array is:";
+	for(i=0;i<n;i++){
+		cout<<arr[i]<<"\t";
+	}
+	return 0;
+}
